04d7c0a117e97f11eb4316297b5ff1d63aa3d40a
AG <asafgo@gmail.com>
2023-08-30 10:04:10 +0300

    Add project files.

---
+++ HamburgerMenuApp.Maui/MauiProgram.cs
@@ -0,0 +1,59 @@
+﻿using HamburgerMenuApp.Core.Interfaces.Services;
+using HamburgerMenuApp.Core.Interfaces.ViewModels;
+using HamburgerMenuApp.Core.ViewModels;
+using HamburgerMenuApp.Maui.Services;
+using HamburgerMenuApp.Maui.Views.Pages;
+using Microsoft.Extensions.Logging;
+using SimpleToolkit.Core;
+using SimpleToolkit.SimpleShell;
+
+namespace HamburgerMenuApp.Maui;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Nunito-Regular.ttf", "NunitoRegular");
+                fonts.AddFont("Nunito-Bold.ttf", "NunitoBold");
+                fonts.AddFont("Nunito-SemiBold.ttf", "NunitoSemiBold");
+            });
+
+        builder.UseSimpleShell();
+        builder.UseSimpleToolkit();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+#if IOS || ANDROID
+        builder.DisplayContentBehindBars();
+#endif
+#if ANDROID
+        builder.SetDefaultStatusBarAppearance(Colors.Transparent, false);
+#endif
+
+        builder.Services.AddTransient<AppShell>();
+
+        builder.Services.AddTransient<AboutUsPage>();
+        builder.Services.AddTransient<ChatPage>();
+        builder.Services.AddTransient<FaqPage>();
+        builder.Services.AddTransient<HomePage>();
+        builder.Services.AddTransient<InviteFriendsPage>();
+        builder.Services.AddTransient<MyPostPage>();
+        builder.Services.AddTransient<NotificationPage>();
+        builder.Services.AddTransient<SupportPage>();
+
+        builder.Services.AddSingleton<INavigationService, NavigationService>();
+
+        builder.Services.AddTransient<IHomePageViewModel, HomePageViewModel>();
+
+        return builder.Build();
+    }
+}
---
+++ HamburgerMenuApp.Maui/Views/Pages/HomePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using HamburgerMenuApp.Core.Interfaces.Services;
+using HamburgerMenuApp.Core.Interfaces.ViewModels;
+using Microsoft.Maui.Controls;
+using SimpleToolkit.SimpleShell;
+
+namespace HamburgerMenuApp.Maui.Views.Pages;
+
+public partial class HomePage : BaseRootContentPage
+{
+	public HomePage(IHomePageViewModel viewModel, INavigationService navigationService) : base(navigationService)
+	{
+		BindingContext = viewModel;
+
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        //MainThread.BeginInvokeOnMainThread(() =>
+        //{
+        //    var shell = SimpleShell.Current;
+        //    var navGrid = shell.FindByName("navBar");
+        //    (navGrid as Microsoft.Maui.Controls.Grid).IsVisible = true;
+        //});
+    }
+}
